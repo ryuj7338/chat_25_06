f9773f8f59419eb5db6667a6d4e64592b638e53f
ryuj7338 <ryuj7338@gmail.com>
2025-06-11 15:49:12 +0900

    메시지를 리스트에 저장

--- src/main/java/com/koreait/exam/chat_25_06/ChatMessage.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatMessage.java
@@ -14,6 +14,14 @@ public class ChatMessage {
     private String content;
 
     public ChatMessage(String authorName, String content) {
-        this(1, LocalDateTime.now(), authorName, content);
+        this(ChatMessageId.getNextId(), LocalDateTime.now(), authorName, content);
+    }
+}
+
+class ChatMessageId {
+    private static long id = 0;
+
+    public static long getNextId() {
+        return ++id;
     }
 }
