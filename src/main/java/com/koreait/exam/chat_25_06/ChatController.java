bbd587237e8b82060f7be1d9f1acc1bbb474ab6e
ryuj7338 <ryuj7338@gmail.com>
2025-06-12 15:19:13 +0900

    messages 필터링

--- src/main/java/com/koreait/exam/chat_25_06/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatController.java
@@ -1,13 +1,16 @@
 package com.koreait.exam.chat_25_06;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
@@ -19,7 +22,6 @@ public class ChatController {
     public record writeChatMessageRequest(String authorName, String content){
 
     }
-
 
     @PostMapping("/writeMessage")
     @ResponseBody
@@ -35,18 +37,41 @@ public class ChatController {
         );
     }
 
+    public record messagesRequest(Long fromId) {
+
+    }
+
     public record messagesResponse(List<ChatMessage> chatMessages, long count) {
 
     }
 
-
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> showMessages() {
+    public RsData<messagesResponse> showMessages(messagesRequest req) {
+
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req: {}", req);
+
+        // 번호가 같이 입력되는 경우
+        if(req.fromId != null){
+
+            // 해당 번호의 채팅 메시지가 전체 리스트의 몇 번째 리스트인지
+            int index = IntStream.range(0, chatMessages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if(index != -1){
+                // 만약, index가 -1이 아니라면 -> 0번부터 index번까지 제거한 리스트 생성
+                messages = chatMessages.subList(index + 1, chatMessages.size());
+            }
+        }
+
         return new RsData<>(
                 "S-1",
                 "성공",
-                new messagesResponse(chatMessages, chatMessages.size())
+                new messagesResponse(messages, messages.size())
         );
     }
 
